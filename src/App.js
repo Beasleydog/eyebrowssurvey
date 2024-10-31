2ddba40a5e64242411a46b50298de4ea69960656
Beasleydog <beasleypero@gmail.com>
2024-10-31 01:34:30 -0400

    add people

--- src/App.js
+++ src/App.js
@@ -2,7 +2,50 @@ import "./App.css";
 import { useState } from "react";
 import Question from "./Question";
 function App() {
-  const images = ["./people/1.png", "./people/2.png"];
+  const images = [
+    "/people/man0.png",
+    "/people/woman0.png",
+    "/people/man1.png",
+    "/people/woman1.png",
+    "/people/man2.png",
+    "/people/woman2.png",
+    "/people/man3.png",
+    "/people/woman3.png",
+    "/people/man4.png",
+    "/people/woman4.png",
+    "/people/man5.png",
+    "/people/woman5.png",
+    "/people/man6.png",
+    "/people/woman6.png",
+    "/people/man7.png",
+    "/people/woman7.png",
+    "/people/man8.png",
+    "/people/woman8.png",
+    "/people/man9.png",
+    "/people/woman9.png",
+    "/people/man10.png",
+    "/people/woman10.png",
+    "/people/man11.png",
+    "/people/woman11.png",
+    "/people/man12.png",
+    "/people/woman12.png",
+    "/people/man13.png",
+    "/people/woman13.png",
+    "/people/man14.png",
+    "/people/woman14.png",
+    "/people/man15.png",
+    "/people/woman15.png",
+    "/people/man16.png",
+    "/people/woman16.png",
+    "/people/man17.png",
+    "/people/woman17.png",
+    "/people/man18.png",
+    "/people/woman18.png",
+    "/people/man19.png",
+    "/people/woman19.png",
+    "/people/man20.png",
+    "/people/woman20.png",
+  ];
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const userId = Math.random().toString(36).substring(2, 15);
   function handleNextQuestion(data) {
@@ -37,7 +80,7 @@ function App() {
     <div className="App">
       {currentQuestion < images.length ? (
         <Question
-          image={images[currentQuestion]}
+          image={`${process.env.PUBLIC_URL}${images[currentQuestion]}`}
           onSubmit={handleNextQuestion}
         />
       ) : (
